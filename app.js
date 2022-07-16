d512b9bb9152dba3fe38184a659234f089c1a4da
darwin.perez <darwin.perez2014@uteq.edu.ec>
2022-07-16 18:22:36 -0500

    El primer commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app= express()
+
+app.get('/', (req, res) => {
+console.log("peticion recibida")
+
+res.send('<h1>Hola Mundo con NODEMON!</h1>')
+
+})
+
+app.listen(4000, () => {
+    console.log('Servidor escuchando en el puerto 4000')
+})
+
